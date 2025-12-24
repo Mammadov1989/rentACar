9340e818538c74e3f45c99245f1c5cb69cc7bed4
m.elnur <48259421+Mammadov1989@users.noreply.github.com>
2025-12-24 17:20:36 +0400

    push

---
+++ RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create;
+public class CreateBrandCommand:IRequest<CreatedBarndResponse>
+{
+    public string Name { get; set; }
+
+    public class CreatedBarndCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBarndResponse>
+    {
+        public Task<CreatedBarndResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBarndResponse response = new CreatedBarndResponse();
+            response.Name = request.Name;
+            response.Id = new Guid();
+            return null;
+        }
+    }
+}
+
